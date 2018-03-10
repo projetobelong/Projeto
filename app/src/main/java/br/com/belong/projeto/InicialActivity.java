373d9b3e27e6d0f0286c5d842d86854c69133298
kilderyfeitosa <kildery27@gmail.com>
2018-03-10 10:13:39 -0300

    Salvando os arquivos no git

---
+++ app/src/main/java/br/com/belong/projeto/InicialActivity.java
@@ -0,0 +1,13 @@
+package br.com.belong.projeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InicialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicial);
+    }
+}
